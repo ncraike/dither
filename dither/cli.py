793777bef0304b57f39901740159f3fa3f32ea38
Nathan Craike <ncraike@gmail.com>
2014-10-31 14:18:40 +1000

    Adds docstrings/descriptions to CLI commands.

--- dither/cli.py
+++ dither/cli.py
@@ -9,19 +9,23 @@ def cli():
     pass
 
 @cli.command()
-@click.pass_context
-def update(context):
-    context.forward(build)
-    context.forward(link)
-
-@cli.command()
 def build():
+    '''Builds new dotfiles from ./dither_templates.'''
     from . import build
     build.build()
 
 @cli.command()
 def link():
+    '''Symlinks latest build into home directory.'''
     from . import link
     link.link(
             base_build_dir=link.BASE_BUILD_DIR,
             home_dir=os.path.expanduser('~'))
+
+@cli.command()
+@click.pass_context
+def update(context):
+    '''Performs the "build" and "link" steps.'''
+    context.forward(build)
+    context.forward(link)
+
