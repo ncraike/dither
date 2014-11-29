6f266bc3817d7ba1095b28f84504c39a18d8d2f4
Nathan Craike <ncraike@gmail.com>
2014-11-29 17:42:51 +1000

    Integration tests: Moves re-usable parts of TestLinkTestCase to class in common.

--- tests/integration/common.py
+++ tests/integration/common.py
@@ -1,5 +1,6 @@
 import os
-import os.path
+
+import unittest
 
 TEMPLATES_DIR = 'dither_templates'
 BUILD_OUTPUT_DIR = 'built_dotfiles'
@@ -45,3 +46,63 @@ def get_context(**kwargs):
     }
 ''')
     return test_context_path
+
+def resolve_symlink(symlink_path, os_module=None):
+    if os_module is None:
+        import os as os_module
+    raw_link_target = os_module.readlink(symlink_path)
+    link_dir = os_module.path.dirname(symlink_path)
+    return os_module.path.join(link_dir, raw_link_target)
+
+class DitherIntegrationTestCase(unittest.TestCase):
+
+    def __init__(self, *args, os_module=None, safe_repr_func=None, **kwargs):
+        if os_module is None:
+            import os as os_module
+        self.os = os_module
+
+        if safe_repr_func is None:
+            from unittest.util import safe_repr as safe_repr_func
+        self.safe_repr = safe_repr_func
+
+        super().__init__(*args, **kwargs)
+
+    def format_fail_msg(self, custom_msg, default_msg):
+        return self._formatMessage(custom_msg, default_msg)
+
+    def failure_exception_with_msg(self, custom_msg, default_msg):
+        formatted_msg = self.format_fail_msg(custom_msg, default_msg)
+        return self.failureException(formatted_msg)
+
+    def assert_is_link_pointing_to(
+            self, link_path, expected_link_target, custom_fail_msg=None):
+        self.assert_is_link(link_path, custom_fail_msg)
+        self.assert_link_points_to(
+                link_path, expected_link_target, custom_fail_msg)
+
+    def assert_is_link(self, link_path, custom_fail_msg=None):
+        exists = self.os.path.exists(link_path)
+        if not exists:
+            raise self.failure_exception_with_msg(
+                    custom_msg=custom_fail_msg,
+                    default_msg="{} does not exist".format(
+                        self.safe_repr(link_path)))
+
+        is_link = self.os.path.islink(link_path)
+        if not is_link:
+            raise self.failure_exception_with_msg(
+                    custom_msg=custom_fail_msg,
+                    default_msg="{} is not a symbolic link".format(
+                        self.safe_repr(link_path)))
+
+    def assert_link_points_to(
+            self, link_path, expected_link_target, custom_fail_msg=None):
+        resolved_link_target = resolve_symlink(link_path, os_module=self.os)
+        points_to = self.os.path.samefile(
+                resolved_link_target, expected_link_target)
+        if not points_to:
+            raise self.failure_exception_with_msg(
+                    custom_msg=custom_fail_msg,
+                    default_msg="Link at {} doesn't point to {}".format(
+                        self.safe_repr(link_path),
+                        self.safe_repr(expected_link_target)))
--- tests/integration/test_link.py
+++ tests/integration/test_link.py
@@ -5,6 +5,7 @@ import unittest
 import dither.link
 
 from common import (
+        DitherIntegrationTestCase,
         setup_test_dither_dir,
         create_test_template,
         create_test_context)
@@ -18,65 +19,7 @@ BUILD_OUTPUT_FILE_NAME = '.testfile'
 LATEST_BUILD_SYMLINK_NAME = 'latest_build'
 INSTALLED_BUILD_SYMLINK_NAME = 'installed_build'
 
-def resolve_symlink(symlink_path, os_module=None):
-    if os_module is None:
-        import os as os_module
-    raw_link_target = os_module.readlink(symlink_path)
-    link_dir = os_module.path.dirname(symlink_path)
-    return os_module.path.join(link_dir, raw_link_target)
-
-class TestLinkTestCase(unittest.TestCase):
-
-    def __init__(self, *args, os_module=None, safe_repr_func=None, **kwargs):
-        if os_module is None:
-            import os as os_module
-        self.os = os_module
-
-        if safe_repr_func is None:
-            from unittest.util import safe_repr as safe_repr_func
-        self.safe_repr = safe_repr_func
-
-        super().__init__(*args, **kwargs)
-
-    def format_fail_msg(self, custom_msg, default_msg):
-        return self._formatMessage(custom_msg, default_msg)
-
-    def failure_exception_with_msg(self, custom_msg, default_msg):
-        formatted_msg = self.format_fail_msg(custom_msg, default_msg)
-        return self.failureException(formatted_msg)
-
-    def assert_is_link_pointing_to(
-            self, link_path, expected_link_target, custom_fail_msg=None):
-        self.assert_is_link(link_path, custom_fail_msg)
-        self.assert_link_points_to(
-                link_path, expected_link_target, custom_fail_msg)
-
-    def assert_is_link(self, link_path, custom_fail_msg=None):
-        exists = self.os.path.exists(link_path)
-        if not exists:
-            raise self.failure_exception_with_msg(
-                    custom_msg=custom_fail_msg,
-                    default_msg="{} does not exist".format(
-                        self.safe_repr(link_path)))
-
-        is_link = self.os.path.islink(link_path)
-        if not is_link:
-            raise self.failure_exception_with_msg(
-                    custom_msg=custom_fail_msg,
-                    default_msg="{} is not a symbolic link".format(
-                        self.safe_repr(link_path)))
-
-    def assert_link_points_to(
-            self, link_path, expected_link_target, custom_fail_msg=None):
-        resolved_link_target = resolve_symlink(link_path, os_module=self.os)
-        points_to = self.os.path.samefile(
-                resolved_link_target, expected_link_target)
-        if not points_to:
-            raise self.failure_exception_with_msg(
-                    custom_msg=custom_fail_msg,
-                    default_msg="Link at {} doesn't point to {}".format(
-                        self.safe_repr(link_path),
-                        self.safe_repr(expected_link_target)))
+class TestLinkTestCase(DitherIntegrationTestCase):
 
     def setUp(self):
         self.sandbox_dir = tempfile.mkdtemp()
