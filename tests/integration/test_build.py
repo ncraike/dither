c4b9fbb22b6451fa9bdffe877a7dd121bfce3f81
Nathan Craike <ncraike@gmail.com>
2014-11-30 16:34:28 +1000

    integration tests: Moves test_build's assertion code for better readability and fail output.

--- tests/integration/test_build.py
+++ tests/integration/test_build.py
@@ -21,22 +21,7 @@ def find_templated_file(build_output_dir):
         raise Exception("Can't find templated file at {!r}".format(filepath))
     return filepath
 
-def is_templated_content_correct(build_output_dir):
-    templated_filepath = find_templated_file(build_output_dir)
-    with open(templated_filepath, 'r') as f:
-        contents = f.read()
-    stripped_contents = '\n'.join(
-            line.strip()
-            for line in
-            contents.splitlines()
-            if line.strip())
-    return bool(
-            stripped_contents == '''\
-sample output
-this line should appear
-My variable is Test value.''')
-
-class Test_build_output_is_correct(
+class Test_build_output_is_as_expected(
         CreateDitherSandboxDirMixin,
         DitherIntegrationTestCase):
 
@@ -51,7 +36,17 @@ class Test_build_output_is_correct(
     def runTest(self):
         dither.build.build()
 
-        assert is_templated_content_correct(self.build_output_dir)
+        templated_filepath = find_templated_file(self.build_output_dir)
+        with open(templated_filepath, 'r') as f:
+            contents = f.read()
+        stripped_contents = '\n'.join(
+                line.strip()
+                for line in contents.splitlines()
+                if line.strip())
+        self.assertEqual(stripped_contents, '''\
+sample output
+this line should appear
+My variable is Test value.''')
 
 if __name__ == '__main__':
     unittest.main()
